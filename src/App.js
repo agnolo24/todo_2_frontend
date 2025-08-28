d4781c4f3d480d0e60545a25d26a2c83f68acf07
amaljith <agnolo2024@gmail.com>
2025-08-28 21:29:54 +0530

    frontend of the todo app with a django backend ans REST as API is finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from "react";
+
+import './App.css'
 
 function App() {
+  let [todo, setTodo] = useState('')
+  let [todos, setTodos] = useState([])
+
+  function fetchTodo() {
+      axios.get('http://127.0.0.1:8000/api/all_todos/').then(
+        (response) => {
+          setTodos(response.data)
+        }
+      )
+    }
+
+  useEffect(
+    () => {
+      fetchTodo()
+    }, []
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center><u><h1>Todo App</h1></u></center>
+
+      <div className="inp-box">
+        <input onChange={
+          (e) => {
+            setTodo(e.target.value)
+          }
+        } value={todo} type="text" />
+
+        <button onClick={
+          () => {
+            axios.post('http://127.0.0.1:8000/api/new_todo/', { title: todo }).then(
+              () => {
+                setTodo(todo = "")
+                fetchTodo()
+              }
+            )
+          }
+        }>+</button>
+      </div>
+
+      <hr />
+
+      <div className="todos">
+        {
+          todos.map(
+            (obj) => {
+              return (
+                <div className='todo'>
+                  <input type="checkbox" checked={obj.status === 'd'} onChange={
+                    () => {
+                      axios.put(`http://127.0.0.1:8000/api/edit_todo/${obj.id}/`, { status: 'd' }).then(
+                        () => {
+                          fetchTodo()
+                        }
+                      )
+                    }
+                  }/>
+                  <h1>{obj.title}</h1>
+                  <button onClick={
+                    () => {
+                      axios.delete(`http://127.0.0.1:8000/api/del_todo/${obj.id}/`).then(
+                        () => {
+                          fetchTodo()
+                        }
+                      )
+                    }
+                  }>X</button>
+                </div>
+              )
+            }
+          )
+        }
+      </div>
     </div>
   );
 }
